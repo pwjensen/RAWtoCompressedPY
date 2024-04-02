ce70d7e55085f96e56e2997f2a1735f65ba8ae7f
afeins19 <56986596+afeins19@users.noreply.github.com>
2024-04-02 20:32:33 +0000

    created models for raw and compressed images

--- Compy/compressor/models.py
+++ Compy/compressor/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
 # Create your models here.
+
+class RawImage(models.Model):
+    # Store the original image
+    image = models.ImageField(upload_to='assets/originals/')
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+
+class CompressedImage(models.Model):
+
+    # Encoding formats available to the user 
+    HUFFMAN = 'Huffman'
+    LZ77 = 'LZ77'
+    RLE = 'RLE'
+
+    ENCODER_CHOICES = {
+        (HUFFMAN, 'Huffman Encoder'),
+        (LZ77, 'LZ77 Encoder'),
+        (RLE, 'RLE Encoder')
+
+    }
+
+    # Foreign Key to the original image
+    original = models.ForeignKey(OriginalImage, on_delete=models.CASCADE, related_name='compressed_images')
+    # Store the compressed image
+    compressed_image = models.ImageField(upload_to='images/compressed/')
+    # Algorithm used for compression
+    algorithm = models.CharField(max_length=50, choices=ENCODER_CHOICES)
+    # Compression metrics like size reduction, quality metrics, etc.
+    size_reduction = models.FloatField()
+    compressed_at = models.DateTimeField(auto_now_add=True)
