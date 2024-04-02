399e5eb3180f4d813390ab3c3a37ae5b3aae6517
afeins19 <56986596+afeins19@users.noreply.github.com>
2024-04-02 22:12:45 +0000

    final models for images have been created

--- Compy/compressor/models.py
+++ Compy/compressor/models.py
@@ -6,28 +6,20 @@ class RawImage(models.Model):
     # Store the original image
     image = models.ImageField(upload_to='assets/originals/')
     uploaded_at = models.DateTimeField(auto_now_add=True)
+    file_size = models.PositiveIntegerField(default=0)  # Stores file size in bytes
 
+    # when an image is saved this automatically populates its file_size 
+    def save(self, *args, **kwargs): 
+        self.file_size = self.image.file.size 
+        super(RawImage, self).save(*args, **kwargs)
 
 class CompressedImage(models.Model):
-
-    # Encoding formats available to the user 
-    HUFFMAN = 'Huffman'
-    LZ77 = 'LZ77'
-    RLE = 'RLE'
-
-    ENCODER_CHOICES = {
-        (HUFFMAN, 'Huffman Encoder'),
-        (LZ77, 'LZ77 Encoder'),
-        (RLE, 'RLE Encoder')
-
-    }
-
     # Foreign Key to the original image
     original = models.ForeignKey(RawImage, on_delete=models.CASCADE, related_name='compressed_images')
     # Store the compressed image
     compressed_image = models.ImageField(upload_to='images/compressed/')
-    # Algorithm used for compression
-    algorithm = models.CharField(max_length=50, choices=ENCODER_CHOICES)
-    # Compression metrics like size reduction, quality metrics, etc.
-    size_reduction = models.FloatField()
+    binary_image = models.TextField(default=0) # binary of huffman encoded image
+    huffman_guide = models.TextField(default=0) # stores the conversion b/w raw val and huffman encoding
+    file_size = models.PositiveIntegerField(default=0)  # Stores file size in bytes
     compressed_at = models.DateTimeField(auto_now_add=True)
+
--- Compy/compressor/views.py
+++ Compy/compressor/views.py
@@ -1,4 +1,27 @@
 from django.shortcuts import render
 
+from django.http import HttpResponse
+from .models import RawImage, CompressedImage
+
 # Create your views here.
+def upload_compress_view(request):
+    if request.method == 'POST': 
+        file = request.FILES['image'] # uploaded file 
+        algorithm = request.POST['algorithm'] # selected algorithm to apply 
+
+
+        original_img = OriginalImage(image=file)
+        original_img.save()
+
+        # handling the file and applying the compression algo
+        # ...
+
+        
+
+        return HttpResponse("Image uploaded and processed using " + algorithm + " algorithm.")
+
+    # user request == 'GET' 
+    algorithm_choices = CompressedImage.ALGORITHM_CHOICES
+    
+    return render(request, 'compressor/upload.html', {'algorithm_choices' : algorithm_choices})
 
